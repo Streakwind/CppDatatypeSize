0d5170f14b88e2ee205120ef1d1610ec2227e762
Streakwind <79235229+Streakwind@users.noreply.github.com>
2021-03-22 11:27:22 -0700

    Update sizelol.cpp

--- sizelol.cpp
+++ sizelol.cpp
@@ -4,11 +4,16 @@
 
 using namespace std;
 
+void Process () {
+    cout << "Size of char: " << sizeof(char) << endl;
+   cout << "Size of int: " << sizeof(int) << endl;
+   cout << "Size of double: " << sizeof(double) << endl;	
+   
+   //read the read me to learn how to add more
+}
+
 int main () {
-   cout << "Size of char : " << sizeof(char) << endl;
-   cout << "Size of int : " << sizeof(int) << endl;
-   cout << "Size of short int : " << sizeof(short int) << endl;
-   cout << "Size of long int : " << sizeof(long int) << endl;
-   cout << "Size of float : " << sizeof(float) << endl;
-   cout << "Size of double : " << sizeof(double) << endl;	
+   Process ();
+   
+   return 0;
 }
